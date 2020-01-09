8200971a3d47d857bdd30aeec40ee56f6ae03f82
Mit <mitpatel.ui.developer@gmail.com>
2020-01-09 09:21:08 -0800

    SQ2 Name

--- src/components/divistions/divistion.js
+++ src/components/divistions/divistion.js
@@ -1,14 +1,6 @@
 
 console.log("SQ2 demo");
 console.log("SQ2 demo");
+My Name is Mit A SVGClipPathElement.i am woking as soiftware developer.
 
-console.log("SQ2 demo");
-
-console.log("SQ2 demo");
-
-console.log("SQ2 demo");
-
-console.log("SQ2 demo");
-
-console.log("SQ2 demo");
 
